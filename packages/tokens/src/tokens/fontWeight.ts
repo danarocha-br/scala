fcf96c4bbf098470fae6dabc8b270106ac46faa3
Dana Rocha <hello@danarocha.com>
2023-05-17 13:59:26 -0300

    :lipstick: (Tokens) font-weight medium update

--- packages/tokens/src/tokens/fontWeight.ts
+++ packages/tokens/src/tokens/fontWeight.ts
@@ -1,6 +1,6 @@
 export const fontWeight = {
   'font-weight-light': '300',
   'font-weight-regular': '400',
-  'font-weight-medium': '500',
+  'font-weight-medium': '600',
   'font-weight-semibold': '700',
 };
